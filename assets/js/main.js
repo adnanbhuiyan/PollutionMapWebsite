256c642a47006db3382cb9bc4c7975a00f3dd9ed
Sumon Ahasan <sumon.ahasan@stonybrook.edu>
2024-10-06 00:44:53 -0400

    added navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,15 @@
 		}
 
 })(jQuery);
+
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
